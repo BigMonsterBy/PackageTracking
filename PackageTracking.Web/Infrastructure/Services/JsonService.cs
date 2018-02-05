c9fdffa3dc818658d4575ee5d3293e8d1c8d53fa
Max <mrudy1999@gmail.com>
2018-02-05 10:37:34 +0300

    Not finished files uploader and JSON parser

---
+++ PackageTracking.Web/Infrastructure/Services/JsonService.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PackageTracking.Web.Infrastructure.Services
+{
+    public static class JsonService
+    {
+        public static T GetObjectsFromJson<T>(string json)
+        {
+            var obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+    }
+}
