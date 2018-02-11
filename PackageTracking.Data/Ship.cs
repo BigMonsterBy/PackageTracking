3d48f5f905a01d637b70ba1efccd8f7a41c42929
BigMonsterBy <bigmonster@inbox.ru>
2018-02-11 15:48:31 +0300

    option for Ship's key

--- PackageTracking.Data/Ship.cs
+++ PackageTracking.Data/Ship.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace PackageTracking.Data
         }
 
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ShipID { get; set; }
         public string Number { get; set; }
         public DateTime SDate { get; set; }
